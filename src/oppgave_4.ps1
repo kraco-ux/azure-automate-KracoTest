f7708ef8cc50233530ef62090bd96159edd0da0e
kraco-ux <dynasti55@hotmail.com>
2022-02-04 15:12:46 +0100

    Løsning_Oppgave4

---
+++ src/oppgave_4.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $UrlKortstokk = "http://nav-deckofcards.herokuapp.com/shuffle"
+)
+
+$ErrorActionPreference = 'Stop'
+
+$response = Invoke-WebRequest -Uri $UrlKortstokk
+
+$cards = $response.Content | ConvertFrom-Json
+
+$kortstokk = @()
+foreach ($card in $cards) {
+    $kortstokk += ($card.suit[0] + $card.value)
+}
+
+Write-Host "Kortstokk: $kortstokk" 
+
+
