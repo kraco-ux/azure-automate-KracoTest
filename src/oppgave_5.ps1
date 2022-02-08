31f7f2ac0bee411ece2d640ce009d9eee914c09b
kraco-ux <dynasti55@hotmail.com>
2022-02-08 09:07:41 +0100

    Løsning Oppgave 5

---
+++ src/oppgave_5.ps1
@@ -0,0 +1,50 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $UrlKortstokk = "http://nav-deckofcards.herokuapp.com/shuffle"
+)
+
+$ErrorActionPreference = 'Stop'
+
+$response = Invoke-WebRequest -Uri $UrlKortstokk
+
+$kortstokk = $response.Content | ConvertFrom-Json
+
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+    [object[]]
+    $kortstokk
+    )
+    $streng = ''
+    foreach ($kort in $kortstokk) {
+    $streng = $streng + "$($kort.suit[0])" + "$($kort.value)" + ","
+    }
+    return $streng.TrimEnd(",")
+    }
+
+    Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+
+function sumPoengKortstokk {
+    [OutputType([int])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+
+    $poengKortstokk = 0 
+
+    foreach ($kort in $kortstokk) {
+        # Undersøk hva en Switch er
+        $poengKortstokk += switch ($kort.value) {
+            { $_ -cin @('J', 'Q', 'K') } { 10 }
+            'A' { 11 }
+            default { $kort.value }
+        }
+    }
+    return $poengKortstokk
+}
+
+Write-Output "Poengsum: $(sumPoengKortstokk -kortstokk $kortstokk)"
+
